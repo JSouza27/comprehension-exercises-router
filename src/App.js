2f1ef69e26cdc438e756968b4c78742b9a63c421
Jonathan Souza <souzajonathan570@gmail.com>
2021-05-07 12:24:00 -0300

    exercicios do 10 ao 13 prontos

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
+import StrictAccess from './StrictAccess';
 
 import { BrowserRouter, Link } from 'react-router-dom';
-import { Route } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/users/:id" render={(props) => <Users {...props} greetingMessage={this.greetingMessage} />} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingMessage={this.greetingMessage} />} />
+          <Route path='/strict-access' render={() => (<StrictAccess user={ {username: 'joao', password: '1234'}} />)} />
+        </Switch>
         <Link to="/about">About</Link>
         <Link to="/users">Users</Link>
+        <Link to="/strict-access">Strict Access</Link>
       </BrowserRouter>
     );
   }
